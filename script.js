16ed8ada8fbbd4acd9a3605de77c3161fb35364f
ashkanjaycob <ashkan.yaghobii@gmail.com>
2023-09-11 23:48:11 +0330

    Number Bug fixed & Input Reset

--- script.js
+++ script.js
@@ -1,44 +1,58 @@
 $(document).ready(function () {
+  const TableBody = $("#TableBody");
+  TableBody.html(localStorage.getItem("dataCustomer"));
 
-  
-    const TableBody = $("#TableBody");
-    TableBody.html(localStorage.getItem('data'));  
-    let Num = 1;
 
-    // main Table Creator Function
-    $("#submitBtn").click(function () {
-        // form preventDefault 
-        $("#form").submit(function (event) {
-            event.preventDefault();
-        })
+//   Save Number Value in localStorage for Next Visit
+  let Num;
+  Num = localStorage.getItem("CustomerNum", Num);
+  if (Num == undefined) {
+    Num = 1;
+    localStorage.setItem("CustomerNum", Num);
+    Num = localStorage.getItem("CustomerNum", Num);
+    console.log(Num);
+  } else {
+    Num = localStorage.getItem("CustomerNum", Num);
+    console.log(Num);
+  }
 
-        const name = $("#FullName").val();
-        const Mail = $("#mail").val();
-        const Phone = $("#Phone").val();
+  // main Table Creator Function
+  $("#submitBtn").click(function () {
+    // form preventDefault
+    $("#form").submit(function (event) {
+      event.preventDefault();
+    });
 
-        console.log(name);
+    const name = $("#FullName");
+    const Mail = $("#mail");
+    const Phone = $("#Phone");
 
-        let TableRow = $(`<tr></tr>`);
-        TableRow.html(`
+    console.log(name);
+
+    let TableRow = $(`<tr></tr>`);
+    TableRow.html(`
         <th>${Num}</th>
-        <td>${name}</td>
-        <td>${Mail}</td>
-        <td>${Phone}</td>
+        <td>${name.val()}</td>
+        <td>${Mail.val()}</td>
+        <td>${Phone.val()}</td>
         <td class="DelI text-danger"><i class="fa fa-close"></i></td>
-        `)
+        `);
 
-        TableBody.append(TableRow);
+    TableBody.append(TableRow);
 
+    localStorage.setItem("dataCustomer", TableBody.html());
+    Num++;
+    localStorage.setItem("CustomerNum", Num);
 
-        localStorage.setItem("data", TableBody.html());
-        Num++;
-    })
-
-
+    function ResetVals() {
+        name.val("") , 
+        Mail.val("") , 
+        Phone.val("")
+    }
+    ResetVals();
+  });
 });
-
 
 // let $opt = $("<option></option>").attr("value", time).text(time);
 
 // $("#time-start").append($opt);
-
