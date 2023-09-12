c635b59e873886363e1d5feb8670252f9a68e52e
ashkanjaycob <ashkan.yaghobii@gmail.com>
2023-09-12 11:26:14 +0330

    Form Validation & Bug fixing

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ $(document).ready(function () {
   const TableBody = $("#TableBody");
   TableBody.html(localStorage.getItem("dataCustomer"));
 
+  const name = $("#FullName");
+  const Mail = $("#mail");
+  const Phone = $("#Phone");
 
-//   Save Number Value in localStorage for Next Visit
+
+  //   Save Number Value in localStorage for Next Visit
   let Num;
   Num = localStorage.getItem("CustomerNum", Num);
   if (Num == undefined) {
@@ -16,42 +20,71 @@ $(document).ready(function () {
     console.log(Num);
   }
 
+
+
+  // form validation in jquery Validation plug-in
+
+
+
+
+
+
   // main Table Creator Function
   $("#submitBtn").click(function () {
+
     // form preventDefault
     $("#form").submit(function (event) {
       event.preventDefault();
+    }).validate({
+      rules: {
+        FullName : "required",
+        Phone: "required",
+        mail: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        FullName : {
+          required : "Please Enter Your FullName.",
+        } ,
+        Phone : {
+          required : "Please Enter Your Phone Number.",
+        } ,
+        mail : {
+          required : "Please Enter an Email.",
+          mail : "Please Enter an <i>Valid </i> Email."
+        }
+      },
+      submitHandler: function(form) {
+        let TableRow = $(`<tr></tr>`);
+        TableRow.html(`
+            <th>${Num}</th>
+            <td>${name.val()}</td>
+            <td>${Mail.val()}</td>
+            <td>${Phone.val()}</td>
+            <td class="DelI text-danger"><i class="fa fa-close"></i></td>
+            `);
+    
+        TableBody.append(TableRow);
+    
+        localStorage.setItem("dataCustomer", TableBody.html());
+        Num++;
+        localStorage.setItem("CustomerNum", Num);
+    
+        function ResetVals() {
+          name.val(""),
+            Mail.val(""),
+            Phone.val("")
+        }
+        ResetVals();
+      }
     });
 
-    const name = $("#FullName");
-    const Mail = $("#mail");
-    const Phone = $("#Phone");
 
-    console.log(name);
-
-    let TableRow = $(`<tr></tr>`);
-    TableRow.html(`
-        <th>${Num}</th>
-        <td>${name.val()}</td>
-        <td>${Mail.val()}</td>
-        <td>${Phone.val()}</td>
-        <td class="DelI text-danger"><i class="fa fa-close"></i></td>
-        `);
-
-    TableBody.append(TableRow);
-
-    localStorage.setItem("dataCustomer", TableBody.html());
-    Num++;
-    localStorage.setItem("CustomerNum", Num);
-
-    function ResetVals() {
-        name.val("") , 
-        Mail.val("") , 
-        Phone.val("")
-    }
-    ResetVals();
   });
 });
+
 
 // let $opt = $("<option></option>").attr("value", time).text(time);
 
